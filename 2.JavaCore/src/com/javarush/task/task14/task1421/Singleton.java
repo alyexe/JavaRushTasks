5468e8d2a215b0dacbe5a7d84b8d56fca6f37bda
Arekusei <kotalexey@gmail.com>
2019-12-24 01:26:14 +0600

    task1421 (Имя константы в задании instance, а не INSTANCE) Java Core, 4 уровень, 8 лекция Singleton

--- 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
@@ -1,10 +1,10 @@
 package com.javarush.task.task14.task1421;
 
 public class Singleton {
-    private static final Singleton INSTANCE = new Singleton();
+    private static final Singleton instance = new Singleton();
 
     public static Singleton getInstance(){
-        return INSTANCE;
+        return instance;
     }
 
     private Singleton() {
