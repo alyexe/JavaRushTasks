8d44529c5f2e907e6a502c589dba8f0f3dc3193c
Arekusei <kotalexey@gmail.com>
2020-07-04 20:29:55 +0600

    task1803 Java Core 8 уровень, 3 лекция Самые частые байты

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task18.task1803;
+
+/* 
+Самые частые байты
+*/
+
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] bytes = new int[255];
+        List<Integer> maxCounts = new LinkedList<>();
+        int max = 0;
+        Scanner in = new Scanner(System.in);
+        String fileName = in.nextLine();
+        FileInputStream fis = new FileInputStream(fileName);
+        while (fis.available() > 0) {
+            int data = fis.read();
+            bytes[data]++;
+        }
+        fis.close();
+
+        for (int i : bytes) {
+            max = Math.max(max, i);
+        }
+
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == max) {
+                maxCounts.add(i);
+            }
+        }
+        for (int i : maxCounts) {
+            System.out.print(i + " ");
+        }
+    }
+}
