234bac9827a734ca419b86389593d5125a66258a
arekusei <kotalexey@gmail.com>
2020-07-22 13:43:28 +0600

    Java Core, 8 уровень, 3 лекция Самые редкие байты

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -1,7 +1,7 @@
-package com.javarush.task.task18.task1803;
+package com.javarush.task.task18.task1804;
 
 /* 
-Самые частые байты
+Самые редкие байты
 */
 
 import java.io.FileInputStream;
@@ -12,24 +12,29 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws Exception {
         int[] bytes = new int[256];
-        List<Integer> maxCounts = new LinkedList<>();
-        int max = 0;
+        List<Integer> minCounts = new LinkedList<>();
+        int min = Integer.MAX_VALUE;
         Scanner in = new Scanner(System.in);
         String fileName = in.nextLine();
         FileInputStream fis = new FileInputStream(fileName);
         while (fis.available() > 0) {
             int data = fis.read();
             bytes[data]++;
-            max = Math.max(max, bytes[data]);
         }
         fis.close();
 
         for (int i = 0; i < bytes.length; i++) {
-            if (bytes[i] == max) {
-                maxCounts.add(i);
+            if (bytes[i] > 0) {
+                min = Math.min(min, bytes[i]);
             }
         }
-        for (int i : maxCounts) {
+
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == min) {
+                minCounts.add(i);
+            }
+        }
+        for (int i : minCounts) {
             System.out.print(i + " ");
         }
     }
